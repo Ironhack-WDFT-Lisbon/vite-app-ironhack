1378a8815abf7b2f3d435ceddaa38a3d4e82fc91
Leah Fenster <leahefen@gmail.com>
2024-08-16 11:55:07 +0100

    show users

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
+import { getUsers } from "./lib";
+import HomePage from "./pages/HomePage";
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers().then((data) => setUsers(data));
+  }, []);
 
   return (
     <>
       <Routes>
-        <Route path="/" element={() => {}} />
+        <Route path="/" element={<HomePage users={users} />} />
       </Routes>
     </>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
-export default function HomePage() {
+export default function HomePage({ users }) {
   return (
     <div>
       <h1>Home Page</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
